d3e31ce1b494eb8cc310ace24a00da9aaff334b4
wangtao <wangtao@iscs.com.cn>
2016-09-01 12:59:05 +0800

    使用request请求图片

--- code/node-img-size/app.js
+++ code/node-img-size/app.js
@@ -1,17 +1,28 @@
 var url = require('url');
 var http = require('http');
- 
+var request = require('request');
+
 var sizeOf = require('image-size');
- 
-var imgUrl = 'http://dn-cnode.qbox.me/FnbE4WGUmsPhktAPMpIeEphgwOkG';
+
+var imgUrl = 'http://o4j806krb.qnssl.com/public/images/cnodejs_light.svg';
 var options = url.parse(imgUrl);
- 
-http.get(options, function (response) {
+
+http.get(options, function(response) {
   var chunks = [];
-  response.on('data', function (chunk) {
+  response.on('data', function(chunk) {
     chunks.push(chunk);
   }).on('end', function() {
     var buffer = Buffer.concat(chunks);
     console.log(sizeOf(buffer));
   });
+});
+
+var requestOptions = {
+  url: imgUrl,
+  encoding: null
+};
+
+request.get(requestOptions, function(err, response, body) {
+  console.log(sizeOf(body));
+  // console.log(body);
 });
