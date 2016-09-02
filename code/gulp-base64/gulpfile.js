29f0f3be0722c6ff984588b7dd44f88ab3e57e05
wangtao <wangtao@iscs.com.cn>
2016-09-02 16:09:37 +0800

    update gulp-base64

---
+++ code/gulp-base64/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var base64 = require('gulp-base64');
+var cssBase64 = require('gulp-css-base64');
+
+gulp.task('copy', function() {
+
+    return gulp.src(['src/*/**','src/index.html'])
+        .pipe(gulp.dest('dist'));;
+});
+
+gulp.task('build', ['copy'], function() {
+
+    return gulp.src('src/app.css')
+          .pipe(base64({
+            debug: true
+          }))
+         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build2', function() {
+    return gulp.src('./*.css')
+        .pipe(base64())
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', function() {
+    return gulp.src('app.css')
+        .pipe(cssBase64())
+        .pipe(gulp.dest('dist'));
+});
