a3508ac7ce41f9accea35571fc81274913ce955d
wangtao <wangtao@iscs.com.cn>
2016-09-12 13:57:08 +0800

    node  http demo

---
+++ code/node-http/http.js
@@ -0,0 +1,48 @@
+var PORT = 3000;
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var mine = require('./mine').types;
+var path = require('path');
+
+var server = http.createServer(function(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    if (pathname.charAt(pathname.length - 1) == "/") {
+        //如果访问目录
+        pathname += "index.html"; //指定为默认网页
+    }
+    var realPath = path.join("assets", pathname);
+    //console.log(realPath);
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function(exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, "binary", function(err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end(err);
+                } else {
+                    var contentType = mine[ext] || "text/plain";
+                    response.writeHead(200, {
+                        'Content-Type': contentType,
+                        'x-power-by':'anytao'
+                    });
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
+    });
+});
+server.listen(PORT);
+console.log("Server runing at port: " + PORT + ".");
