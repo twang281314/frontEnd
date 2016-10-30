28814ca3e1be1b6d154811fe071df8bc816619d3
wangtao <wangtao@iscs.com.cn>
2016-10-30 16:04:16 +0800

    error

--- code/node-c++/hello/hello.cc
+++ code/node-c++/hello/hello.cc
@@ -1,14 +1,6 @@
 #include <node.h>
 #include <v8.h>
 
-float forfunc(){
-  float i,j=0;
-  for(i=0;i<10000000;i++){
- j += i/2;
-  };
-  return j;
-}
-
 void Method(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
   v8::HandleScope scope(isolate);
@@ -16,9 +8,7 @@ void Method(const v8::FunctionCallbackInfo<v8::Value>& args) {
 }
 
 void init(v8::Local<v8::Object> exports) {
-  float j;
-  j = forfunc();
   NODE_SET_METHOD(exports, "hello", Method);
 }
 
-NODE_MODULE(hello, init);
+NODE_MODULE(binding, init);
