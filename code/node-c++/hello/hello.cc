c742e56fe868539bf795a8450618a1c7ab5ebecb
wangtao <wangtao@iscs.com.cn>
2016-10-30 14:56:48 +0800

    update

--- code/node-c++/hello/hello.cc
+++ code/node-c++/hello/hello.cc
@@ -1,17 +1,14 @@
-#include <node/v8.h>
-#include <node/node.h>
-
-using namespace node;
-using namespace v8;
+#include <node.h>
+#include <v8.h>
 
 void Method(const v8::FunctionCallbackInfo<v8::Value>& args) {
-v8::Isolate* isolate = args.GetIsolate();
-v8::HandleScope scope(isolate);
-args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "Hello world"));
+  v8::Isolate* isolate = args.GetIsolate();
+  v8::HandleScope scope(isolate);
+  args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "world"));
 }
 
-void init(v8::Local<v8::Object> target) {
-NODE_SET_METHOD(target, "hello", Method);
+void init(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
+  NODE_SET_METHOD(module, "exports", Method);
 }
 
 NODE_MODULE(binding, init);
