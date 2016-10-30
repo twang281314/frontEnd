29c838a6843b512bdb372c2f64d9696fd5cb98bd
wangtao <wangtao@iscs.com.cn>
2016-10-30 14:18:33 +0800

    node c++

---
+++ code/node-c++/hello/hello.cc
@@ -0,0 +1,16 @@
+#include <node.h>
+#include <v8.h>
+
+using namespace v8;
+
+Handle<Value> Method(const Arguments& args) {
+  HandleScope scope;
+  return scope.Close(String::New("hello,world"));
+}
+
+void init(Handle<Object> exports) {
+  exports->Set(String::NewSymbol("hello"),
+      FunctionTemplate::New(Method)->GetFunction());
+}
+
+NODE_MODULE(hello, init)
