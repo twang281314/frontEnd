811490f5436f3de1d75004114e0777370b90d5e9
anytao <twang281314@gmail.com>
2018-07-25 16:20:37 +0800

    数据源动态切换

--- code/spring-boot/eop/src/main/java/com/eptison/eop/modules/sys/service/impl/SysUserServiceImpl.java
+++ code/spring-boot/eop/src/main/java/com/eptison/eop/modules/sys/service/impl/SysUserServiceImpl.java
@@ -3,15 +3,19 @@ package com.eptison.eop.modules.sys.service.impl;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.eptison.eop.datasources.DataSourceNames;
+import com.eptison.eop.datasources.annotation.DataSource;
 import com.eptison.eop.modules.sys.dao.SysUserDao;
 import com.eptison.eop.modules.sys.entity.SysUserEntity;
 import com.eptison.eop.modules.sys.service.SysUserService;
 import org.springframework.stereotype.Service;
 
+
 @Service("sysUserService")
 public class SysUserServiceImpl extends ServiceImpl<SysUserDao, SysUserEntity> implements SysUserService {
 
     @Override
+    @DataSource(name= DataSourceNames.SECOND)
     public SysUserEntity queryByUserName(String username) {
         return baseMapper.queryByUserName(username);
     }
